42924baedd0423e908f31ba67ac3f740cedfbdcb
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-04-24 15:41:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CAVE-Camera-Rig-Prefab
---
+++ files/multidisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class multidisplay : MonoBehaviour {
+
+    Camera[] multiDisplay = new Camera[4];
+    void Start()
+    {
+        //find cameras and assign to multiDisplay
+        multiDisplay[0] = GameObject.Find("FrontRightCam").GetComponent<Camera>();
+        multiDisplay[1] = GameObject.Find("LeftCam").GetComponent<Camera>();
+        multiDisplay[2] = GameObject.Find("FrontLeftCam").GetComponent<Camera>();
+        multiDisplay[3] = GameObject.Find("RightCam").GetComponent<Camera>();
+
+        //set and enable the displays
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            multiDisplay[i].targetDisplay = i; 
+            Display.displays[i].Activate(); 
+        }
+    }
+}
